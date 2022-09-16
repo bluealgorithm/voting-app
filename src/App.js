92c654c44c8f179c0e3e21a7835f029ab879a2a2
femmyte <sanyaoluadefemi@gmail.com>
2022-09-16 12:21:41 +0100

    wrapped all the components in the Router and AuthContext

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Signin from "./Components/auth/Signin";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Components/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import PublicRoute from "./utils/PublicRoute";
+import PrivateRoute from "./utils/PrivateRoute";
+import Navbar from "./Components/Navbar";
+import Account from "./Pages/Account";
+import MenInTech from "./Pages/MenInTech";
+import WomenInTech from "./Pages/WomenInTech";
+// const url = "http://localhost:5000/verifyToken?token=";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/account" element={<PrivateRoute />}>
+            <Route exact path="/account" element={<Account />}></Route>
+          </Route>
+          <Route exact path="/men-in-tech" element={<PrivateRoute />}>
+            <Route exact path="/men-in-tech" element={<MenInTech />}></Route>
+          </Route>
+          <Route exact path="/women-in-tech" element={<PrivateRoute />}>
+            <Route
+              exact
+              path="/women-in-tech"
+              element={<WomenInTech />}
+            ></Route>
+          </Route>
+          <Route exact path="/signin" element={<PublicRoute />}>
+            <Route exact path="/signin" element={<Signin />} />
+          </Route>
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
